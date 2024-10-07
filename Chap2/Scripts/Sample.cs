2730c9f3fb2fcedcc27acfd9c25f9b9add5cacc5
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2024-10-07 11:07:54 +0900

    Chap2 Hello World

---
+++ Chap2/Scripts/Sample.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hello, Worldをコンソールウィンドウに表示する
+        Debug.Log("Hello, World!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
